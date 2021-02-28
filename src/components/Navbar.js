4f2adfaee9385542a850cc8caab279cae5abb18e
cizordj <32869222+cizordj@users.noreply.github.com>
2021-02-28 15:05:35 -0300

    Fix the navbar at the top
    - Also add transparency as the background

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import {AppBar, AppBarItem, AppBarMenu} from 'metro4-react';
+import { AppBar, AppBarItem, AppBarMenu } from 'metro4-react';
+import '../css/navbar.css';
 
 class Navbar extends Component {
     render() {
         return (
-            <AppBar>
-                <AppBarItem>
-                    <span className="text-leader">Zé Pintor</span>
-                </AppBarItem>
-                <AppBarMenu>
-                    <AppBarItem>Início</AppBarItem> 
-                    <AppBarItem>Contato</AppBarItem> 
-                    <AppBarItem>Sobre</AppBarItem> 
-                </AppBarMenu>
-            </AppBar>
+            <div className="container-fluid bg-gradient pos-fixed fixed-top">
+                <AppBar cls="bg-transparent fg-white container pos-relative">
+                    <AppBarItem>
+                        <span className="text-leader">Zé Pintor</span>
+                    </AppBarItem>
+                    <AppBarMenu cls="ml-auto text-upper">
+                        <AppBarItem>Início</AppBarItem>
+                        <AppBarItem>Contato</AppBarItem>
+                        <AppBarItem>Sobre</AppBarItem>
+                    </AppBarMenu>
+                </AppBar>
+            </div>
         )
     }
 }
