0be57c4d43ea3c87ab3edde809cff67292b34500
cizordj <32869222+cizordj@users.noreply.github.com>
2021-02-28 15:06:18 -0300

    Update reception component
    - Update the foreground color

--- src/components/Reception.js
+++ src/components/Reception.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class Reception extends Component {
     render() {
         return (
-            <div className="display4 fg-black text-center">
+            <div className="display4 fg-white text-center">
                 <span className="reduce-3 enlarge-1-md">{this.props.title}</span>
                 <h4>{this.props.subtitle}</h4>
             </div>
