3a862aba57254ae40249800e69096c44dce6fc70
cizordj <32869222+cizordj@users.noreply.github.com>
2021-02-28 15:07:01 -0300

    Change the background color for the whole site
    - Now the background color is a gradient

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import Home from './pages/index';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import './css/body.css';
+import Contato from './pages/Contato';
+import Sobre from './pages/Sobre';
 class App extends Component {
     render() {
         return (
@@ -12,6 +14,8 @@ class App extends Component {
                 <Switch>
                     <Route path="/">
                         <Home />
+                        <Contato />
+                        <Sobre />
                     </Route>
                 </Switch>
             </Router>
