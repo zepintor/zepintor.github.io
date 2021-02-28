2a3cb4a87800d5f6de6c5bd36ff490979d0c3a2a
cizordj <32869222+cizordj@users.noreply.github.com>
2021-02-28 11:58:50 -0300

    Add new component Reception
    This component is responsible for printing the big title in the middle
    of the screen

---
+++ src/components/Reception.js
@@ -0,0 +1,14 @@
+import { InfoPanelTitle } from 'metro4-react';
+import React, { Component } from 'react';
+
+class Reception extends Component {
+    render() {
+        return (
+            <div className="display4 fg-black text-center">
+                <span className="reduce-3 enlarge-1-md">{this.props.title}</span>
+                <h4>{this.props.subtitle}</h4>
+            </div>
+        )
+    }
+}
+export default Reception;
