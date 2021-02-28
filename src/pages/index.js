ec015ac658a6d29aa888ca603b0c6fe57b241ed3
cizordj <32869222+cizordj@users.noreply.github.com>
2021-02-28 15:07:46 -0300

    Add three cards to the front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import { Grid } from 'metro4-react'
+import Reception from '../components/Reception';
+import Card from "../components/Card";
 
 class Home extends Component {
     render() {
         return (
-            <Grid className="h-100 d-flex flex-align-center flex-justify-center">
-                <h1>Hello World!</h1>
-                <p className="text-leader">
-                    Welcome to Metro 4 for React App template!
-                    </p>
-            </Grid>
+            <>
+                <Grid className="h-100 d-flex flex-align-center flex-justify-center">
+                    <Reception title="Pinturas em Indaial"
+                        subtitle="Orçamento sem compromisso" />
+                </Grid>
+                <Grid className="h-100 bg-white d-flex flex-align-center flex-justify-center">
+                    <div className="container">
+                        <div className="feature-table row">
+                            <>
+                            <Card title="Capricho"
+                            subtitle="Serviços com bom acabamento"
+                            button_caption="Mostre-me"
+                            background="blue"
+                            icon="mif-hammer"
+                            button_background="info"/>
+
+                            <Card title="Visão"
+                            subtitle="Boas ideias para cores"
+                            button_caption="Quero ver"
+                            background="steel"
+                            icon="mif-eye"
+                            button_background="secondary"/>
+
+                            <Card title="Nota fiscal"
+                            subtitle="Para empresas"
+                            button_caption="Como assim?"
+                            background="amber"
+                            icon="mif-barcode"
+                            button_background="yellow"/>
+                            </>
+                        </div>
+                    </div>
+                </Grid>
+            </>
         )
     }
 }
