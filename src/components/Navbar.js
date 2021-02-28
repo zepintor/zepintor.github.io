971e4bff1584a4b5258a5a6db8442a0ade28160a
cizordj <32869222+cizordj@users.noreply.github.com>
2021-02-28 10:15:43 -0300

    Add the Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {AppBar} from 'metro4-react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <AppBar>
+                <a href="#" class="brand no-hover">
+                    <span style="width: 55px;" class="p-2 border bd-dark border-radius">
+                        m<sup>4</sup>
+                    </span>
+                </a>
+
+                <ul class="app-bar-menu">
+                    <li><a href="#">Home</a></li>
+                    <li>
+                        <a href="#" class="dropdown-toggle">Products</a>
+                        <ul class="d-menu" data-role="dropdown">
+                            <li><a href="#">Windows 10</a></li>
+                            <li><a href="#">Office 365</a></li>
+                            <li class="divider bg-lightGray"></li>
+                            <li><a href="#">Skype</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contacts</a></li>
+                </ul>
+            </AppBar>
+        )
+    }
+}
+export default Navbar;
