3966aade8e7d3af1b6b7f3fb2922594bb64dc5b8
cizordj <32869222+cizordj@users.noreply.github.com>
2021-03-01 20:02:00 -0300

    Update links in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import { AppBar, AppBarItem, AppBarMenu } from 'metro4-react';
 import '../css/navbar.css';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
     render() {
         return (
             <div className="container-fluid pos-fixed fixed-top fg-white bg-gradient z-top">
                 <AppBar cls="bg-transparent container pos-relative">
-                    <AppBarItem>
-                        <span className="text-leader">Zé Pintor</span>
-                    </AppBarItem>
-                    <AppBarMenu cls="ml-auto text-upper">
-                        <AppBarItem>Início</AppBarItem>
-                        <AppBarItem>Contato</AppBarItem>
-                        <AppBarItem>Sobre</AppBarItem>
+                    <AppBarMenu collapsed="false">
+                        <Link to="/">
+                            <AppBarItem isBrand cls="mr-auto">
+                                <span className="text-leader text-cap">Zé Pintor</span>
+                            </AppBarItem>
+                        </Link>
+                    </AppBarMenu>
+                    <AppBarMenu cls="text-upper ml-auto">
+                        <Link to="/">
+                            <AppBarItem>Início</AppBarItem>
+                        </Link>
+                        <Link to="/contato">
+                            <AppBarItem>Contato</AppBarItem>
+                        </Link>
+                        <Link to="/sobre">
+                            <AppBarItem>Sobre</AppBarItem>
+                        </Link>
                     </AppBarMenu>
                 </AppBar>
             </div>
