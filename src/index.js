d9efa497ad5dd1f9c8ad9498999086bae9af5355
cizordj <32869222+cizordj@users.noreply.github.com>
2021-03-02 20:12:02 -0300

    Add route for the new page

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Sobre from './pages/sobre/';
 import NotFound from './pages/404/';
 import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
+import Madeiras from './pages/madeiras';
 
 class App extends Component {
     render() {
@@ -26,6 +27,18 @@ class App extends Component {
                     </Route>
                     <Route exact path="/sobre">
                         <Sobre />
+                    </Route>
+                    <Route exact path="/madeiras">
+                        <Madeiras/>
+                    </Route>
+                    <Route exact path="/pinturas">
+
+                    </Route>
+                    <Route exact path="/telhado">
+
+                    </Route>
+                    <Route exact path="/gesso">
+
                     </Route>
                     <Route exact path="*">
                         <NotFound />
